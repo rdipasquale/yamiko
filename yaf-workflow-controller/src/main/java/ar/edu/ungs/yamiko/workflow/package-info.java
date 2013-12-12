b62b6b0387d2b903cdb79171b8822f3ed444e853
rdipasquale <ricardo.dipasquale@gmail.com>
2013-12-11 23:09:20 -0300

    Doc

---
+++ yaf-workflow-controller/src/main/java/ar/edu/ungs/yamiko/workflow/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Clases de utilización general en el flujo de trabajo de algoritmos genéticos del framework Yamiko.
+ */
+package ar.edu.ungs.yamiko.workflow;
---
+++ yaf-workflow-controller/src/main/java/ar/edu/ungs/yamiko/workflow/serial/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Implementación del flujo de trabajo de algoritmos genéticos del framework Yamiko.
+ */
+package ar.edu.ungs.yamiko.workflow.serial;
