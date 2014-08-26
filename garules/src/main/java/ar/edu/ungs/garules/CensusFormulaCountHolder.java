1ccdb49da39ecc789b526a4bdecc751f062476a9
ricardo <ricardo.dipasquale@gmail.com>
2014-08-26 00:54:05 -0300

    FormulaCountHolder

---
+++ garules/src/main/java/ar/edu/ungs/garules/CensusFormulaCountHolder.java
@@ -0,0 +1,13 @@
+package ar.edu.ungs.garules;
+
+/**
+ * Esta clase tiene la responsabilidad de explorar los resultados del job para proveerle los valores al fitness evaluator.
+ *
+ */
+public class CensusFormulaCountHolder {
+
+	public long getFormulaCount(String formula)
+	{
+		return 0;
+	}
+}
