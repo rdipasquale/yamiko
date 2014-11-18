1ca9d115da730e44e565a47448743797850ece3e
ricardo <ricardo.dipasquale@gmail.com>
2014-11-17 23:45:07 -0300

    Unique

--- yaf-gadomain/src/main/java/ar/edu/ungs/yamiko/ga/operators/impl/UniqueIntegerPopulationInitializer.java
+++ yaf-gadomain/src/main/java/ar/edu/ungs/yamiko/ga/operators/impl/UniqueIntegerPopulationInitializer.java
@@ -17,10 +17,17 @@ import ar.edu.ungs.yamiko.ga.toolkit.StaticHelper;
  */
 public class UniqueIntegerPopulationInitializer implements PopulationInitializer<Integer[]>{
 
-	private int maxZeros;
+	private int maxZeros=1;
+	private boolean startWithZero=false;
 	
-	
-	
+	public boolean isStartWithZero() {
+		return startWithZero;
+	}
+
+	public void setStartWithZero(boolean startWithZero) {
+		this.startWithZero = startWithZero;
+	}
+
 	public int getMaxZeros() {
 		return maxZeros;
 	}
@@ -30,18 +37,27 @@ public class UniqueIntegerPopulationInitializer implements PopulationInitializer
 	}
 
 	public void execute(Population<Integer[]> population) {
+
 		if (population==null) return;
 		
 		for (int i=0;i<population.size();i++)
 		{
+			int zeros=0;
 			Set<Integer> verificador=new HashSet<Integer>();
 			Integer[] numeros=new Integer[population.getGenome().size()];
-			for (int j=0;j<population.getGenome().size();j++)
+			if (startWithZero)
+			{
+				zeros++;
+				numeros[0]=0;
+			}
+			for (int j=zeros;j<population.getGenome().size();j++)
 			{
 				Integer rand=StaticHelper.randomInt(population.getGenome().size());
-				while (verificador.contains(rand)) rand=StaticHelper.randomInt(population.getGenome().size());
+				while (zeros>maxZeros || verificador.contains(rand))
+					rand=StaticHelper.randomInt(population.getGenome().size());
 				verificador.add(rand);
-				numeros[j]=rand;				
+				numeros[j]=rand;
+				if (rand==0) zeros++;
 			}
 			population.addIndividual(IntegerStaticHelper.create(population.getGenome().getStructure().keySet().iterator().next(),numeros));
 		}
