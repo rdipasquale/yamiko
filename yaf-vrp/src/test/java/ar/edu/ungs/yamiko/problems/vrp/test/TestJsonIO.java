79b83e46b4673a0002832f719e6242a005d2dc03
ricardo <ricardo.dipasquale@gmail.com>
2015-05-25 21:38:33 -0300

    Armado de un file de pruebas

--- yaf-vrp/src/test/java/ar/edu/ungs/yamiko/problems/vrp/test/TestJsonIO.java
+++ yaf-vrp/src/test/java/ar/edu/ungs/yamiko/problems/vrp/test/TestJsonIO.java
@@ -9,7 +9,17 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.graphhopper.GHRequest;
+import com.graphhopper.GHResponse;
+import com.graphhopper.GraphHopper;
+import com.graphhopper.routing.AlgorithmOptions;
+import com.graphhopper.routing.util.AllEdgesIterator;
+import com.graphhopper.routing.util.EncodingManager;
+import com.graphhopper.util.EdgeExplorer;
+import com.graphhopper.util.EdgeIterator;
+
 import ar.edu.ungs.yamiko.problems.vrp.entities.TrafficData;
+import ar.edu.ungs.yamiko.problems.vrp.utils.TruckFlagEncoder;
 
 @SuppressWarnings("deprecation")
 public class TestJsonIO {
@@ -33,6 +43,26 @@ public class TestJsonIO {
 		System.out.println(td2);
 		Assert.assertEquals(td, td2);
 		
+	}
+	
+	@Test
+	public void testMakeFileImport()
+	{
+		Long ts=System.currentTimeMillis();
+		GraphHopper hopper = new GraphHopper().forServer();
+		//hopper.setCHEnable(enable)disableCHShortcuts();
+		hopper.setInMemory();
+		hopper.setOSMFile(System.getProperty("user.home")+"/gps/buenos-aires_argentina.osm");
+		hopper.setGraphHopperLocation(System.getProperty("user.home")+"/gps/graph/truck");
+		hopper.setEncodingManager(new EncodingManager(new TruckFlagEncoder()));
+
+		hopper.importOrLoad();
+
+		EdgeExplorer explorer = hopper.getGraph().createEdgeExplorer();
+		EdgeIterator iter = explorer.setBaseNode(1);
+		while(iter.next()) {			
+			System.out.println(iter.getEdge() + " - " + iter.getDistance() + " - [" + iter.getBaseNode() + "," + iter.getAdjNode() + "]");
+		}
 	}
 
 }
