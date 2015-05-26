6859794918968458d2426e25340f77a664ac382d
ricardo <ricardo.dipasquale@gmail.com>
2015-05-25 23:42:19 -0300

    Generación de eventos de tráfico básica concluida.

--- yaf-vrp/src/main/java/ar/edu/ungs/yamiko/problems/vrp/utils/GPSHelper.java
+++ yaf-vrp/src/main/java/ar/edu/ungs/yamiko/problems/vrp/utils/GPSHelper.java
@@ -1,5 +1,7 @@
 package ar.edu.ungs.yamiko.problems.vrp.utils;
 
+import java.util.Random;
+
 import org.gavaghan.geodesy.Ellipsoid;
 import org.gavaghan.geodesy.GeodeticCalculator;
 import org.gavaghan.geodesy.GeodeticCurve;
@@ -27,4 +29,32 @@ public class GPSHelper {
 		return geoCurve.getEllipsoidalDistance();
 	}
 	
+	/**
+	 * Devuelve un punto en el mapa en cercanías del punto de latitud lat y longitud lon con un radio 'rad' km 
+	 * @param lon
+	 * @param lat
+	 * @param rad
+	 */
+	public static Double[] getRandomPointInMap(double lat ,double lon,  int rad) {
+	    Random random = new Random();
+
+	    // Convert radius from meters to degrees
+	    double radiusInDegrees = rad / 111000f;
+
+	    double u = random.nextDouble();
+	    double v = random.nextDouble();
+	    double w = radiusInDegrees * Math.sqrt(u);
+	    double t = 2 * Math.PI * v;
+	    double x = w * Math.cos(t);
+	    double y = w * Math.sin(t);
+
+	    // Adjust the x-coordinate for the shrinking of the east-west distances
+	    double new_x = x / Math.cos(lat);
+
+	    double foundLongitude = new_x + lon;
+	    double foundLatitude = y + lat;
+	    //System.out.println("Longitude: " + foundLongitude + "  Latitude: " + foundLatitude );
+	    
+	    return new Double[]{foundLatitude,foundLongitude};
+	}
 }
--- yaf-vrp/src/test/java/ar/edu/ungs/yamiko/problems/vrp/test/TestJsonIO.java
+++ yaf-vrp/src/test/java/ar/edu/ungs/yamiko/problems/vrp/test/TestJsonIO.java
@@ -1,6 +1,8 @@
 package ar.edu.ungs.yamiko.problems.vrp.test;
 
 import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Iterator;
 
 import junit.framework.Assert;
 
@@ -17,8 +19,11 @@ import com.graphhopper.routing.util.AllEdgesIterator;
 import com.graphhopper.routing.util.EncodingManager;
 import com.graphhopper.util.EdgeExplorer;
 import com.graphhopper.util.EdgeIterator;
+import com.graphhopper.util.Instruction;
+import com.graphhopper.util.InstructionList;
 
 import ar.edu.ungs.yamiko.problems.vrp.entities.TrafficData;
+import ar.edu.ungs.yamiko.problems.vrp.utils.GPSHelper;
 import ar.edu.ungs.yamiko.problems.vrp.utils.TruckFlagEncoder;
 
 @SuppressWarnings("deprecation")
@@ -32,7 +37,6 @@ public class TestJsonIO {
 	public void tearDown() throws Exception {
 	}
 
-	@SuppressWarnings("deprecation")
 	@Test
 	public void testJSonIO() throws Exception {
 		ObjectMapper om=new ObjectMapper();
@@ -46,22 +50,76 @@ public class TestJsonIO {
 	}
 	
 	@Test
-	public void testMakeFileImport()
+	public void testMakeFileImport() throws Exception
 	{
-		Long ts=System.currentTimeMillis();
+		ObjectMapper om=new ObjectMapper();
 		GraphHopper hopper = new GraphHopper().forServer();
 		//hopper.setCHEnable(enable)disableCHShortcuts();
 		hopper.setInMemory();
 		hopper.setOSMFile(System.getProperty("user.home")+"/gps/buenos-aires_argentina.osm");
 		hopper.setGraphHopperLocation(System.getProperty("user.home")+"/gps/graph/truck");
 		hopper.setEncodingManager(new EncodingManager(new TruckFlagEncoder()));
-
 		hopper.importOrLoad();
+		
+		
+//
+//		EdgeExplorer explorer = hopper.getGraph().createEdgeExplorer();
+//		EdgeIterator iter = explorer.setBaseNode(1);
+//		while(iter.next()) {			
+//			System.out.println(iter.getEdge() + " - " + iter.getDistance() + " - [" + iter.getBaseNode() + "," + iter.getAdjNode() + "]");
+//		}
+		
+		int camiones=40;
+		int viajes=10;
+		Double[] desde;
+		Double[] hasta;
+		
+		for (int c=1;c<camiones+1;c++)
+		{
+			Calendar cal=Calendar.getInstance();
+			cal.set(Calendar.HOUR_OF_DAY, 8);
+			cal.set(Calendar.MINUTE, 0);
+			cal.set(Calendar.SECOND, 0);
+			cal.set(Calendar.MILLISECOND, 0);
+			hasta=GPSHelper.getRandomPointInMap(-34.621475, -58.460379, 8950);
+			
+			for (int v=1;v<viajes+1;v++)
+			{
+				desde=hasta;
+				hasta=GPSHelper.getRandomPointInMap(-34.621475, -58.460379, 8950);
+				
+				GHRequest req = new GHRequest(desde[0],desde[1],hasta[0],hasta[1]).setVehicle("truck").setWeighting("fastest").setAlgorithm(AlgorithmOptions.ASTAR_BI);
+				GHResponse rsp = hopper.route(req);
 
-		EdgeExplorer explorer = hopper.getGraph().createEdgeExplorer();
-		EdgeIterator iter = explorer.setBaseNode(1);
-		while(iter.next()) {			
-			System.out.println(iter.getEdge() + " - " + iter.getDistance() + " - [" + iter.getBaseNode() + "," + iter.getAdjNode() + "]");
+				// first check for errors
+				if(rsp.hasErrors()) {
+					desde=new Double[]{-34.644838, -58.360684};
+					hasta=new Double[]{-34.563170, -58.485310};
+					req = new GHRequest(desde[0],desde[1],hasta[0],hasta[1]).setVehicle("truck").setWeighting("fastest").setAlgorithm(AlgorithmOptions.ASTAR_BI);
+					rsp = hopper.route(req);
+					
+					// rsp.getErrors()
+				   //return;
+				}
+
+
+				InstructionList il = rsp.getInstructions();
+				Iterator<Instruction> i=il.iterator();
+				while (i.hasNext())
+				{
+					Instruction ins=i.next();
+					cal.add(Calendar.MILLISECOND, new Long(ins.getTime()).intValue());
+					Double speed=30d;
+					if (ins.toString().contains("AU")) speed=80d;
+					if (ins.toString().contains("Av.")) speed=50d;
+					if (ins.toString().contains("Avenida")) speed=50d;
+					TrafficData td=new TrafficData(c, new Timestamp(cal.getTimeInMillis()), ins.getPoints().getLatitude(0), ins.getPoints().getLongitude(0), speed, ins.toString(), true, cal.get(Calendar.WEEK_OF_YEAR), cal.get(Calendar.DAY_OF_WEEK));
+					String salida=om.writeValueAsString(td);
+					System.out.println(salida);
+				}
+						
+				
+			}
 		}
 	}
 
