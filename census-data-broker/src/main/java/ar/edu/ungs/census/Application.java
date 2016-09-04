d577eba818fe57f062b54605aa9a28b91514a6b7
rdipasquale <ricardo.dipasquale@gmail.com>
2016-09-03 21:37:47 -0300

    Organize Imports

--- census-data-broker/src/main/java/ar/edu/ungs/census/Application.java
+++ census-data-broker/src/main/java/ar/edu/ungs/census/Application.java
@@ -1,15 +1,9 @@
 package ar.edu.ungs.census;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCache;
-import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
--- census-data-broker/src/main/java/ar/edu/ungs/census/CensusBroker.java
+++ census-data-broker/src/main/java/ar/edu/ungs/census/CensusBroker.java
@@ -5,7 +5,6 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
