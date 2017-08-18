865666b6bcada19ed4bc14bd11e21b7c4d724df0
rdipasquale <ricardo.dipasquale@gmail.com>
2017-08-18 16:42:07 -0300

    Sail....

---
+++ yaf-sail/src/mail/scala/ar/edu/ungs/sail/Domain.scala
@@ -0,0 +1,19 @@
+package ar.edu.ungs.sail
+
+/**
+ * Este trait modela cualquier especificación de Polar VMG. Cada instancia concreta es la especifiación propia de una embarcación concreta.
+ */
+@SerialVersionUID(1L)
+trait VMG extends Serializable{
+  
+  /**
+   * Devuelve la velocidad a un ángulo específico respecto de la incidencia del viento y con una velocidad específica de viento.
+   */
+  def getSpeed(angle:Int, windspeed:Int):Double
+  
+  /**
+   * Busca en el polar correspondiente a la velocidad pasada como parámetro, la velocidad más alta. Devuelve un par ordenado con el ángulo y la velocidad
+   */
+  def getMaxSpeed(windspeed:Int):(Int,Double)
+  
+}
