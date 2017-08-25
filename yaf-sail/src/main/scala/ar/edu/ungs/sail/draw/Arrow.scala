853b3775f0ce123e6d73ac4d931d0fdbf9505a1e
rdipasquale <ricardo.dipasquale@gmail.com>
2017-08-25 01:47:37 -0300

    Arrows

---
+++ yaf-sail/src/main/scala/ar/edu/ungs/sail/draw/Arrow.scala
@@ -0,0 +1,43 @@
+package ar.edu.ungs.sail.draw
+
+import javax.imageio.ImageIO
+import java.io.File
+import java.awt.Graphics2D
+import java.awt.geom.AffineTransform
+import java.awt.image.BufferedImage
+
+object png{def image=ImageIO.read(new File("Arrow.png"))}
+
+class Arrow(percentLength:Double,angle:Double,cuadrant:(Int,Int),g:Graphics2D,canvas:BufferedImage,n:Int) {
+
+  val image=png.image
+  def draw()={
+       val at:AffineTransform = new AffineTransform();
+       
+      // 4. translate it to the center of the component
+       val xCuadrante=cuadrant._1*canvas.getWidth()/n
+       val yCuadrante=(n-cuadrant._2-1)*canvas.getHeight()/n
+      val centerX=10+(Math.abs(Math.sin(angle*Math.PI/180))*canvas.getWidth()/(2*n)).intValue()
+      val centerY=10+(Math.abs(Math.cos(angle*Math.PI/180))*canvas.getHeight()/(2*n)).intValue()
+      at.translate(xCuadrante+centerX,yCuadrante +centerY)
+//      at.translate(image.getWidth/2,image.getHeight/2);
+       
+      // 3. do the actual rotation
+       val rad=angle*Math.PI/180
+      at.rotate(rad);
+
+      // 2. just a scale because this image is big
+      at.scale(0.8*percentLength/100, 0.5);
+
+      // 1. translate the object so that you rotate it around the 
+      //    center (easier :))
+//      val centerX=canvas.getWidth()/(2*n)
+//      val centerY=canvas.getHeight()/(2*n)
+//      val centerX=(-1)*image.getWidth()/2
+//      val centerY=(-1)*image.getHeight()/2
+//      at.translate(centerX, centerY);
+      
+      g.drawImage(image,at, null)
+  }
+  
+}
---
+++ yaf-sail/src/test/scala/ar/edu/ungs/sail/test/DrawTest.scala
@@ -0,0 +1,99 @@
+package ar.edu.ungs.sail.test
+
+import java.awt.BasicStroke
+import java.awt.Color
+import java.awt.Font
+import java.awt.geom.Ellipse2D
+import java.awt.geom.Line2D
+import java.awt.geom.Rectangle2D
+import java.awt.image.BufferedImage
+
+import org.junit.Before
+import org.junit.Test
+import javax.imageio.ImageIO
+import java.io.File
+import java.awt.Graphics2D
+import java.awt.geom.Path2D
+import java.awt.RenderingHints
+import java.awt.geom.AffineTransform
+import java.awt.Image
+import ar.edu.ungs.sail.draw.Arrow
+
+@Test
+class DrawTest {
+
+  	@Before
+  	def setUp()=
+  	{
+  	} 
+  	
+    @Test
+  	def testDraw = {  
+       // Size of image
+      val size = (609, 611)
+      
+      // create an image
+      val canvas = new BufferedImage(size._1, size._2, BufferedImage.TYPE_INT_RGB)
+//      val canvas = ImageIO.read(new File("4x4.png"));
+
+      // get Graphics2D for the image
+      
+      val g = canvas.createGraphics()
+      g.drawImage(ImageIO.read(new File("4x4.png")), null, 0, 0)
+      
+      // clear background
+//      g.setColor(Color.WHITE)
+ //     g.fillRect(0, 0, canvas.getWidth, canvas.getHeight)
+      
+      // enable anti-aliased rendering (prettier lines and circles)
+      // Comment it out to see what this does!
+      g.setRenderingHint(java.awt.RenderingHints.KEY_ANTIALIASING,  java.awt.RenderingHints.VALUE_ANTIALIAS_ON)
+      
+      // draw two filled circles
+//      g.setColor(Color.RED)
+//      g.fill(new Ellipse2D.Double(30.0, 30.0, 40.0, 40.0))
+//      g.fill(new Ellipse2D.Double(230.0, 380.0, 40.0, 40.0))
+      
+      // draw an unfilled circle with a pen of width 3
+//      g.setColor(Color.MAGENTA)
+//      g.setStroke(new BasicStroke(3f))
+//      g.draw(new Ellipse2D.Double(400.0, 35.0, 30.0, 30.0))
+      
+      // draw a filled and an unfilled Rectangle
+//      g.setColor(Color.CYAN)
+//      g.fill(new Rectangle2D.Double(20.0, 400.0, 50.0, 20.0))
+//      g.draw(new Rectangle2D.Double(400.0, 400.0, 50.0, 20.0))
+      
+      // draw a line
+//      g.setStroke(new BasicStroke())  // reset to default
+//      g.setColor(new Color(0, 0, 255)) // same as Color.BLUE
+//      g.draw(new Line2D.Double(50.0, 50.0, 250.0, 400.0))
+      
+      // draw some text
+//      g.setColor(new Color(0, 128, 0)) // a darker green
+//      g.setFont(new Font("Batang", Font.PLAIN, 20))
+//      g.drawString("Hello World!", 155, 225)
+
+      
+//      val arrows:Array[Arrow] = Array(new LineArrow())
+//      draw(g,50,50,arrows)
+      
+      val vientos=Array(  ((0,0),125,60),((0,1),135,70),((0,2),145,80),((0,3),150,90),
+                          ((1,0),115,50),((1,1),125,40),((1,2),145,30),((1,3),10,90),
+                          ((2,0),95,60),((2,1),105,50),((2,2),115,40),((2,3),110,30),
+                          ((3,0),90,35),((3,1),100,45),((3,2),190,45),((3,3),210,40)
+                          )
+      vientos.foreach(f=>new Arrow(f._3,f._2,f._1,g,canvas,4).draw())                    
+  
+
+      // done with drawing
+      g.dispose()
+      
+      // write image to a file
+      javax.imageio.ImageIO.write(canvas, "png", new java.io.File("drawing.png"))  	
+      
+      assert(true)
+      }	
+
+
+}      
