80f9fd1434affd81b6042e2b3e1f5e58422cf879
rdipasquale <ricardo.dipasquale@gmail.com>
2017-08-27 01:41:05 -0300

    mas escenarios

---
+++ yaf-sail/src/main/scala/ar/edu/ungs/sail/GenerarEscenarios.scala
@@ -0,0 +1,27 @@
+package ar.edu.ungs.sail
+
+import ar.edu.ungs.sail.draw.Graficador
+import ar.edu.ungs.sail.simulation.WindSimulation
+import ar.edu.ungs.serialization.Serializador
+import ar.edu.ungs.serialization.SerializadorEscenario
+
+object GenerarEscenarios extends App {
+  
+  override def main(args : Array[String]) {
+      val nodoInicial:Nodo=new Nodo("Inicial - (0)(1)",List((0,0)),null)
+      val nodoFinal:Nodo=new Nodo("Final - (195)(199)",List((3,3)),null)
+      val rioDeLaPlata:Cancha=new CanchaRioDeLaPlata(50,4,50,nodoInicial,nodoFinal);
+      val t0=WindSimulation.generarEstadoInicial(50, 270, 14, 6, 3)
+      Serializador.run("estadoInicialEscenario50x50.winds", t0)
+      val salida=WindSimulation.simular(rioDeLaPlata, t0, 120, 0, 0, 5.5, 2.3, false)
+      SerializadorEscenario.run("escenario50x50_0.txt", "1",salida)
+      salida.foreach(f=>Graficador.draw(50, 4, f._2, "escenario50x50_t" + f._1 + ".png", 35))
+
+      1 to 1000 foreach(i=>{
+        val salida2=WindSimulation.simular(rioDeLaPlata, t0, 120, 0, 0, 5.5, 2.3, false)
+        SerializadorEscenario.run("escenario50x50_"+i+".txt", i.toString(),salida2)      
+      })
+      
+  }
+ 
+}
--- yaf-sail/src/main/scala/ar/edu/ungs/serialization/SerializationFrameworkSail.scala
+++ yaf-sail/src/main/scala/ar/edu/ungs/serialization/SerializationFrameworkSail.scala
@@ -5,8 +5,9 @@ import java.io._
 object SerializadorEscenario {
   def run(file:String,id:String,escenario:List[(Int, List[((Int, Int), Int, Int, Int)])]):Any={  
     val bw = new BufferedWriter(new FileWriter(new File(file)))
-    escenario.foreach(f=>f._2.foreach(g=>bw.write(id+","+f._1+g._1._1+","+g._1._2+","+g._2+","+g._3+","+g._4+"\n")))
+    escenario.foreach(f=>f._2.foreach(g=>bw.write(id+","+f._1+","+g._1._1+","+g._1._2+","+g._2+","+g._3+","+g._4+"\n")))
     bw.close()
     true
   }
+  
 }
