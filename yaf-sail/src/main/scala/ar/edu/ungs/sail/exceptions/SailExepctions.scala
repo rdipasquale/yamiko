671b75e3fe96cfd2bd7cf65c05550638adee08a4
rdipasquale <ricardo.dipasquale@gmail.com>
2018-02-13 11:37:30 -0300

    Operador de Crossover propuesto por He Fangguo para una tira de nodos de longitud variable (Int,Int)

---
+++ yaf-sail/src/main/scala/ar/edu/ungs/sail/exceptions/SailExepctions.scala
@@ -0,0 +1,6 @@
+package ar.edu.ungs.sail.exceptions
+
+import ar.edu.ungs.yamiko.ga.exceptions.YamikoException
+
+
+class NotCompatibleIndividualException(message:String) extends YamikoException(message:String){}
--- yaf-sail/src/main/scala/ar/edu/ungs/sail/operators/SailCrossovers.scala
+++ yaf-sail/src/main/scala/ar/edu/ungs/sail/operators/SailCrossovers.scala
@@ -6,6 +6,7 @@ import ar.edu.ungs.yamiko.ga.exceptions.NullIndividualException
 import scala.util.Random
 import java.util.BitSet
 import ar.edu.ungs.yamiko.ga.toolkit.IndividualBitSetJavaFactory
+import ar.edu.ungs.sail.exceptions.NotCompatibleIndividualException
 
 
 /**
@@ -60,7 +61,61 @@ class SailOnePointCrossover extends Crossover[List[(Int,Int)]] {
 class SailPathOnePointCrossoverHeFangguo extends Crossover[List[(Int,Int)]] {
 
     override def execute(individuals:List[Individual[List[(Int,Int)]]]):List[Individual[List[(Int,Int)]]] = {
-      return null;
+		  if (individuals==null) throw new NullIndividualException("SailPathOnePointCrossoverHeFangguo")
+		  if (individuals.length<2) throw new NullIndividualException("SailPathOnePointCrossoverHeFangguo")
+		  if (individuals(0)==null || individuals(1)==null) throw new NullIndividualException("SailPathOnePointCrossoverHeFangguo");
+		  
+		  val i1 = individuals(0)
+		  val i2 = individuals(1)
+		
+		  val c1=i1.getGenotype().getChromosomes()(0).getFullRawRepresentation()
+		  val c2=i2.getGenotype().getChromosomes()(0).getFullRawRepresentation()
+		
+		  val realSize1=i1.getGenotype().getChromosomes()(0).getFullSize()
+		  val realSize2=i2.getGenotype().getChromosomes()(0).getFullSize()
+      
+		  val intersect=c1.intersect(c2)
+		  
+		  if (intersect.size<=2) throw new NotCompatibleIndividualException("SailPathOnePointCrossoverHeFangguo")
+		  
+		  val point=1+Random.nextInt(intersect.size-2)
+
+		  var desc1=c1.slice(0, c1.indexOf(intersect(point)) )++c2.slice(c2.indexOf(intersect(point)), c2.length)
+		  var desc2=c2.slice(0, c2.indexOf(intersect(point)) )++c1.slice(c1.indexOf(intersect(point)), c1.length)
+
+		  // Eliminar ciclos)		  
+      var duplicados1=desc1.groupBy(identity).collect { case (x, List(_,_,_*)) => x }
+		  while (duplicados1.size>0)
+		  {
+		    val ciclomax=(duplicados1.map(f => {
+		      val p1=desc1.indexOf(f)
+		      val p2=desc1.indexOf(f,p1+1)
+		      (f,p2-p1)		      
+		    })).maxBy(_._2)
+	      val p1=desc1.indexOf(ciclomax._1)
+	      val p2=desc1.indexOf(ciclomax._1,p1+1)
+		    desc1=desc1.take(p1)++desc1.takeRight(desc1.size-p2)
+		    duplicados1=desc1.groupBy(identity).collect { case (x, List(_,_,_*)) => x }
+		  }
+
+		  var duplicados2=desc2.groupBy(identity).collect { case (x, List(_,_,_*)) => x }
+		  while (duplicados2.size>0)
+		  {
+		    val ciclomax=(duplicados2.map(f => {
+		      val p1=desc2.indexOf(f)
+		      val p2=desc2.indexOf(f,p1+1)
+		      (f,p2-p1)		      
+		    })).maxBy(_._2)
+	      val p1=desc2.indexOf(ciclomax._1)
+	      val p2=desc2.indexOf(ciclomax._1,p1+1)
+		    desc2=desc2.take(p1)++desc2.takeRight(desc2.size-p2)
+		    duplicados2=desc2.groupBy(identity).collect { case (x, List(_,_,_*)) => x }
+		  }
+		  
+	    val d1:Individual[List[(Int,Int)]]= IndividualPathFactory.create(i1.getGenotype().getChromosomes()(0).name(), desc1 )
+	    val d2:Individual[List[(Int,Int)]]= IndividualPathFactory.create(i2.getGenotype().getChromosomes()(0).name(), desc2 )
+		
+		  return List(d1,d2)		      
     }
 }
 
