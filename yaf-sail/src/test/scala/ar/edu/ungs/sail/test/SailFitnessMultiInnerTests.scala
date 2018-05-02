106cf588b0c21179d2b3f0ffd563e718c27e739c
rdipasquale <ricardo.dipasquale@gmail.com>
2018-05-01 22:26:09 -0300

    detalle de documentación

--- yaf-sail/src/test/scala/ar/edu/ungs/sail/test/SailFitnessMultiInnerTests.scala
+++ yaf-sail/src/test/scala/ar/edu/ungs/sail/test/SailFitnessMultiInnerTests.scala
@@ -345,105 +345,121 @@ class SailFitnessMultiInnerTest extends Serializable{
      		println("Evaluar 2000 toma " + (t4-t3) + "ms")
 		    val fitnessResults2=fitnessResults.sortBy(f=>if (f._2==null) 0d else f._2)
 
-		    // Tomo los 100 mejores
+		    // Tomo los 100 mejores para imprimir
 		    fitnessResults2.reverse.take(100).foreach(f=>println(f._2 + " - " + f._1))
 		    
 		    assertTrue(fitM>=fitnessResults2.reverse.take(1)(0)._2)
       
     }
-    
-    
-    // StackOverflowError
-//    @Test
-//    def testVerifyAllPaths()=
-//    {
-//      val t1=System.currentTimeMillis()
-//
-//      val cancha2:Cancha=new CanchaRioDeLaPlataUniManiobra(4,4,50,nodoInicial,nodoFinal,null);
-//      val g2=cancha2.getGraph()
-//  		val path  = new Stack[g2.NodeT]()
-//      var seen = new HashSet[g2.NodeT]()
-//      val cache= new HashMap[g2.NodeT,Boolean]()
-//      
-//      println(cancha2.getArcos().size)
-//      
-//  		val v:g2.NodeT=g2 get cancha2.getNodoInicial()
-//  		val t:g2.NodeT=g2 get cancha2.getNodoFinal()
-//      
-//          
-//      def stuck(x:g2.NodeT):Boolean={
-//  		   if (x.equals(v)) return false
-//         if (x.equals(t)) return false
-//         for (y <- x.neighbors.toList.sortWith((x, y) => math.abs(t.getX()-x.getX())+math.abs(t.getY()-x.getY()) < math.abs(t.getX()-y.getX())+math.abs(t.getY()-y.getY())))
-//         {
-//           if (!seen.contains(y)) seen.add(y)
-//           if (!stuck(y)) 
-//             {
-//               cache.put(x, false)
-//               return false
-//             }
-//         }
-//  		   cache.put(x, true)
-//         true
-//      }
-//
-//      def search(x:g2.NodeT):Unit={
-//         if (x.equals(t)) println(path)          
-//         seen = new HashSet[g2.NodeT]()
-//         path.foreach(f=>seen.add(f))
-//         if (stuck(x)) return
-//         for (y <- x.neighbors--seen)
-//         {
-//          path.push(y)
-//          search(y)
-//          path.pop
-//         }
-//      }
-//    
-//      
-//      path.push(v)
-//      search(v)
-//    }
 
+    //Se imprime una salida de validacion
+    //------------------------------------
+    //Fitness del individio 'diagonal'= 9947.016235351562
+    //Fitness del mejor camino en t0 = 9975.316524505615
+    //
+    //Se encontraron 80211 paths de longitud <=9 en 9.166 segundos
+    //A buscar paths mejores.... (fit=9975.316524505615)
+    //Evaluar 2000 toma 410219ms
+    //9887.668434143066 - ListBuffer(105, 18, 24, 26, 52, 55, 81, 106)
+    //9886.940979003906 - ListBuffer(105, 18, 24, 37, 52, 55, 81, 106)
+    //9886.100875854492 - ListBuffer(105, 18, 24, 26, 52, 67, 81, 106)
+    //9882.30143737793 - ListBuffer(105, 18, 26, 37, 52, 67, 81, 106)
+    //9881.235412597656 - ListBuffer(105, 18, 23, 24, 37, 38, 55, 81, 106)
+    //9879.83415222168 - ListBuffer(105, 18, 25, 49, 61, 55, 81, 106)
+    //9879.34487915039 - ListBuffer(105, 18, 23, 25, 26, 52, 55, 81, 106)
+    //9879.275527954102 - ListBuffer(105, 18, 23, 24, 26, 52, 67, 81, 106)
+    //9878.862350463867 - ListBuffer(105, 18, 26, 52, 38, 55, 81, 106)
+    //9878.775451660156 - ListBuffer(105, 18, 26, 38, 52, 55, 81, 106)
+    //9878.69376373291 - ListBuffer(105, 18, 24, 25, 37, 52, 55, 81, 106)
+    //9878.617416381836 - ListBuffer(105, 18, 23, 25, 26, 52, 62, 81, 106)
+    //9878.562744140625 - ListBuffer(105, 18, 25, 24, 42, 38, 55, 81, 106)
+    //9877.75894165039 - ListBuffer(105, 18, 25, 23, 42, 43, 55, 81, 106)
+    //9877.241821289062 - ListBuffer(105, 18, 23, 24, 26, 52, 78, 81, 106)
+    //9877.189331054688 - ListBuffer(105, 18, 25, 26, 37, 38, 55, 81, 106)
+    //9876.461868286133 - ListBuffer(105, 18, 25, 37, 26, 43, 55, 81, 106)
+    //9876.235816955566 - ListBuffer(105, 18, 23, 41, 26, 38, 55, 81, 106)
+    //9875.868408203125 - ListBuffer(105, 18, 23, 37, 26, 52, 62, 81, 106)
+    //9875.73738861084 - ListBuffer(105, 18, 26, 23, 42, 43, 55, 81, 106)
+    //9875.703468322754 - ListBuffer(105, 18, 26, 38, 54, 55, 81, 106)
+    //9875.511177062988 - ListBuffer(105, 18, 24, 25, 49, 43, 55, 81, 106)
+    //9875.398040771484 - ListBuffer(105, 18, 26, 43, 54, 55, 81, 106)
+    //9874.89729309082 - ListBuffer(105, 18, 23, 26, 42, 52, 55, 81, 106)
+    //9874.546463012695 - ListBuffer(105, 18, 26, 49, 66, 67, 81, 106)
+    //9874.38737487793 - ListBuffer(105, 18, 24, 36, 49, 38, 55, 81, 106)
+    //9874.140739440918 - ListBuffer(105, 18, 23, 26, 49, 38, 55, 81, 106)
+    //9874.140739440918 - ListBuffer(105, 18, 23, 26, 49, 38, 55, 81, 106)
+    //9873.841697692871 - ListBuffer(105, 18, 24, 25, 42, 52, 78, 81, 106)
+    //9873.413276672363 - ListBuffer(105, 18, 23, 49, 37, 38, 55, 81, 106)
+    //9873.103019714355 - ListBuffer(105, 18, 26, 52, 54, 67, 81, 106)
+    //9873.05004119873 - ListBuffer(105, 18, 23, 37, 42, 52, 67, 81, 106)
+    //9872.988372802734 - ListBuffer(105, 18, 23, 26, 50, 43, 55, 81, 106)
+    //9872.665405273438 - ListBuffer(105, 18, 23, 26, 51, 38, 55, 81, 106)
+    //9872.653678894043 - ListBuffer(105, 18, 25, 48, 37, 38, 55, 81, 106)
+    //9872.649528503418 - ListBuffer(105, 18, 24, 26, 49, 52, 55, 81, 106)
+    //9872.512756347656 - ListBuffer(105, 18, 26, 49, 66, 78, 81, 106)
+    //9872.06021118164 - ListBuffer(105, 18, 24, 47, 26, 52, 62, 81, 106)
+    //9872.014282226562 - ListBuffer(105, 18, 25, 26, 50, 43, 55, 81, 106)
+    //9872.014282226562 - ListBuffer(105, 18, 26, 37, 42, 43, 55, 81, 106)
+    //9871.950103759766 - ListBuffer(105, 18, 23, 26, 52, 38, 55, 81, 106)
+    //9871.921569824219 - ListBuffer(105, 18, 26, 52, 62, 55, 81, 106)
+    //9871.912887573242 - ListBuffer(105, 18, 23, 24, 49, 66, 55, 81, 106)
+    //9871.845733642578 - ListBuffer(105, 18, 23, 37, 49, 52, 55, 81, 106)
+    //9871.74380493164 - ListBuffer(105, 18, 23, 26, 42, 52, 78, 81, 106)
+    //9871.599090576172 - ListBuffer(105, 18, 25, 26, 49, 52, 55, 81, 106)
+    //9871.517593383789 - ListBuffer(105, 18, 25, 24, 49, 52, 78, 81, 106)
+    //9871.517578125 - ListBuffer(105, 18, 25, 24, 49, 61, 67, 81, 106)
+    //9871.420822143555 - ListBuffer(105, 18, 23, 26, 50, 52, 62, 81, 106)
+    //9871.194610595703 - ListBuffer(105, 18, 24, 49, 37, 52, 62, 81, 106)
+    //9871.174194335938 - ListBuffer(105, 18, 26, 42, 37, 52, 55, 81, 106)
+    //9871.174194335938 - ListBuffer(105, 18, 25, 26, 50, 52, 55, 81, 106)
+    //9871.092681884766 - ListBuffer(105, 18, 24, 37, 42, 52, 78, 81, 106)
+    //9870.871627807617 - ListBuffer(105, 18, 25, 26, 49, 52, 62, 81, 106)
+    //9870.802276611328 - ListBuffer(105, 18, 24, 37, 49, 52, 67, 81, 106)
+    //9870.774871826172 - ListBuffer(105, 18, 23, 37, 38, 52, 55, 81, 106)
+    //9870.774871826172 - ListBuffer(105, 18, 23, 26, 38, 52, 62, 81, 106)
+    //9870.539474487305 - ListBuffer(105, 18, 26, 23, 49, 61, 55, 81, 106)
+    //9870.223495483398 - ListBuffer(105, 18, 24, 42, 49, 52, 55, 81, 106)
+    //9870.147155761719 - ListBuffer(105, 18, 23, 49, 42, 52, 55, 81, 106)
+    //9869.81901550293 - ListBuffer(105, 18, 25, 24, 49, 66, 67, 81, 106)
+    //9869.81381225586 - ListBuffer(105, 18, 23, 24, 26, 29, 55, 81, 106)
+    //9869.81381225586 - ListBuffer(105, 18, 24, 23, 26, 29, 55, 81, 106)
+    //9869.81201171875 - ListBuffer(105, 18, 23, 37, 49, 61, 55, 81, 106)
+    //9869.80078125 - ListBuffer(105, 18, 25, 26, 38, 52, 62, 81, 106)
+    //9869.747833251953 - ListBuffer(105, 18, 26, 36, 49, 52, 55, 81, 106)
+    //9869.73567199707 - ListBuffer(105, 18, 25, 36, 49, 61, 55, 81, 106)
+    //9869.496032714844 - ListBuffer(105, 18, 24, 26, 49, 61, 67, 81, 106)
+    //9869.419692993164 - ListBuffer(105, 18, 26, 23, 49, 61, 67, 81, 106)
+    //9869.416870117188 - ListBuffer(105, 18, 23, 46, 42, 52, 55, 81, 106)
+    //9869.396301269531 - ListBuffer(105, 18, 25, 37, 51, 52, 55, 81, 106)
+    //9869.337203979492 - ListBuffer(105, 18, 26, 51, 75, 78, 81, 106)
+    //9869.250595092773 - ListBuffer(105, 18, 23, 37, 51, 52, 67, 81, 106)
+    //9869.07113647461 - ListBuffer(105, 18, 24, 26, 50, 52, 78, 81, 106)
+    //9868.99478149414 - ListBuffer(105, 18, 23, 42, 50, 52, 62, 81, 106)
+    //9868.953964233398 - ListBuffer(105, 18, 24, 48, 49, 43, 55, 81, 106)
+    //9868.933547973633 - ListBuffer(105, 18, 26, 48, 42, 38, 55, 81, 106)
+    //9868.838729858398 - ListBuffer(105, 18, 26, 51, 37, 43, 55, 81, 106)
+    //9868.759399414062 - ListBuffer(105, 18, 23, 24, 49, 66, 78, 81, 106)
+    //9868.737121582031 - ListBuffer(105, 18, 26, 50, 37, 52, 55, 81, 106)
+    //9868.69223022461 - ListBuffer(105, 18, 23, 37, 49, 52, 78, 81, 106)
+    //9868.643280029297 - ListBuffer(105, 18, 26, 52, 77, 67, 81, 106)
+    //9868.425186157227 - ListBuffer(105, 18, 24, 26, 52, 61, 55, 81, 106)
+    //9868.413024902344 - ListBuffer(105, 18, 24, 26, 51, 61, 62, 81, 106)
+    //9868.314575195312 - ListBuffer(105, 18, 24, 49, 50, 43, 55, 81, 106)
+    //9868.267761230469 - ListBuffer(105, 18, 25, 48, 42, 52, 67, 81, 106)
+    //9868.18978881836 - ListBuffer(105, 18, 26, 24, 49, 66, 62, 81, 106)
+    //9868.122619628906 - ListBuffer(105, 18, 26, 37, 49, 52, 62, 81, 106)
+    //9868.113876342773 - ListBuffer(105, 18, 24, 48, 49, 52, 55, 81, 106)
+    //9868.032852172852 - ListBuffer(105, 18, 24, 26, 43, 52, 78, 81, 106)
+    //9868.020690917969 - ListBuffer(105, 18, 26, 37, 42, 52, 78, 81, 106)
+    //9868.020690917969 - ListBuffer(105, 18, 26, 37, 42, 52, 78, 81, 106)
+    //9867.991607666016 - ListBuffer(105, 18, 24, 49, 51, 38, 55, 81, 106)
+    //9867.944351196289 - ListBuffer(105, 18, 23, 42, 51, 52, 62, 81, 106)
+    //9867.932861328125 - ListBuffer(105, 18, 23, 46, 49, 43, 55, 81, 106)
+    //9867.697723388672 - ListBuffer(105, 18, 24, 26, 52, 61, 62, 81, 106)
+    //9867.697723388672 - ListBuffer(105, 18, 24, 37, 52, 61, 55, 81, 106)
+    //9867.430191040039 - ListBuffer(105, 18, 24, 26, 52, 53, 67, 81, 106)
+    //9867.338653564453 - ListBuffer(105, 18, 23, 14, 26, 38, 55, 81, 106)
+    //9867.305389404297 - ListBuffer(105, 18, 26, 37, 50, 52, 67, 81, 106)
     
-//    @Test
-//    def testVerifyAllPaths()=
-//    {
-//      //def negWeight(e: g.EdgeT,t:Int): Float = Costo.calcCostoEsc(e._1,e._2,cancha.getMetrosPorLadoCelda(),cancha.getNodosPorCelda(), f._2.getEstadoByTiempo(t) ,barco)		
-//
-//  		
-//  		val pathResult:ListBuffer[(g.EdgeT,Float)]=ListBuffer()
-//
-//  		var v:g.NodeT=g get cancha.getNodoInicial()
-//  		var t:g.NodeT=g get cancha.getNodoFinal()
-//
-//  		var path  = new Stack[g.NodeT]()
-//      var onPath  = new HashSet[g.NodeT]()
-//      
-//      enumerate(v,t)
-//      
-//      def enumerate(v:g.NodeT,t:g.NodeT):Boolean={
-//        path.push(v)
-//        onPath.add(v)
-//  
-//        if (v.equals(t)) 
-//              println(path.reverse);
-//        else 
-//          if (path.length>300)
-//          {
-//            path.clear()
-//            onPath.clear()
-//            return false
-//          }
-//          else
-//            v.diSuccessors.foreach(s=>if (!onPath.contains(s)) if (!enumerate(s, t)) return false)
-//  
-//          // done exploring from v, so remove from path
-//          path.pop()
-//          onPath.remove(v)    
-//          true
-//      }
-//    }
-
 
 }      
