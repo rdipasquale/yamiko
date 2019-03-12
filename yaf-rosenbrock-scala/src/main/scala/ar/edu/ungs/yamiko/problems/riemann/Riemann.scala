1f5678b0eaeee6290ce986ed575e3e43cdc2beb9
rdipasquale <rdipasquale@RDIPASQUALE.buenos-aires.cammesa.com>
2019-03-12 16:28:33 -0300

    riemann

--- yaf-rosenbrock-scala/src/main/scala/ar/edu/ungs/yamiko/problems/riemann/Riemann.scala
+++ yaf-rosenbrock-scala/src/main/scala/ar/edu/ungs/yamiko/problems/riemann/Riemann.scala
@@ -4,22 +4,16 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 
 object Riemann extends App {
-    
   override def main(args : Array[String]) {
     val URI_SPARK="local[8]"
     val XINICIAL=0d;
     val XFINAL=10d;
     val PASOS=100;
     val INCREMENTO=(XFINAL-XINICIAL)/PASOS
-    
-    println(URI_SPARK)
   	val conf=new SparkConf().setMaster(URI_SPARK).setAppName("Riemann")
     val sc:SparkContext=new SparkContext(conf)
   
   	val valores=sc.parallelize(List.range(1, PASOS).map(f=>XINICIAL+INCREMENTO*f))
   	val integral=valores.map(f=>(Math.pow(f,2)+Math.pow(f+INCREMENTO,2))/2).sum
-  	
-  	println(integral)
   }	
- 
 }
