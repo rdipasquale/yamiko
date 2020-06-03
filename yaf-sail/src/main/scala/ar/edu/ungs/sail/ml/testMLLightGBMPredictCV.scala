1272687f003b824f1c34f3b2846dc5e7996c33b4
rdipasquale <ricardo.dipasquale@gmail.com>
2020-06-02 21:06:10 -0300

    mas cosas

--- yaf-sail/src/main/scala/ar/edu/ungs/sail/ml/testMLLightGBMPredictCV.scala
+++ yaf-sail/src/main/scala/ar/edu/ungs/sail/ml/testMLLightGBMPredictCV.scala
@@ -18,12 +18,16 @@ import org.apache.spark.ml.tuning.CrossValidator
 import com.microsoft.ml.spark.LightGBMUtils
 import com.microsoft.ml.spark.LightGBMClassificationModel
 import com.microsoft.ml.spark.ComputeModelStatistics
+import com.microsoft.ml.spark.LightGBMBooster
+import com.microsoft.ml.spark.LightGBMBase
+import com.microsoft.ml.spark.LightGBMRegressor
+import com.microsoft.ml.spark.LightGBMRegressionModel
 
-object testMLGBT2 {
+object testMLGBTPredictCV {
   def main(args: Array[String]): Unit = {
     
     
-  	val URI_SPARK="local[4]"
+  	val URI_SPARK="local[8]"
     val conf=new SparkConf().setMaster(URI_SPARK)
     val spark = SparkSession
       .builder
@@ -33,36 +37,37 @@ object testMLGBT2 {
     
     // Load the data stored in LIBSVM format as a DataFrame.
   //val data = spark.read.format("libsvm").load("/media/ricardo/hd1/Ricardo/UNGS/DSO2020/1/features3.csv")
-  val dataOrig = spark.read.format("libsvm").load("/media/ricardo/hd1/Ricardo/UNGS/DSO2020/2/training.txt")
+  val data= spark.read.format("libsvm").load("/media/ricardo/hd1/Ricardo/UNGS/DSO2020/2/trainingPredict.txt")
   
-  val data2=dataOrig.rdd.map(d=>(if(d.getDouble(0)==0) 0 else 1,d.get(1).asInstanceOf[SparseVector]))
-  val data=spark.createDataFrame(data2).toDF("label","features")
 
   data.printSchema()
+  data.show()
+//  
+//  val paramGrid = new ParamGridBuilder()
+//  .addGrid(hashingTF.numFeatures, Array(10, 100, 1000))
+//  .addGrid(lr.regParam, Array(0.1, 0.01))
+//  .build()
+//  
+//  val pipeline = new Pipeline()
+//  .setStages(Array(tokenizer, hashingTF, lr))
+//  
+//val cv = new CrossValidator()
+//  .setEstimator(pipeline)
+//  .setEvaluator(new BinaryClassificationEvaluator)
+//  .setNumFolds(5)  // Use 3+ in practice
+//  .setParallelism(2)  // Evaluate up to 2 parameter settings in parallel  
+//  
+//  
   
-// Index labels, adding metadata to the label column.
-// Fit on whole dataset to include all labels in index.
-//val labelIndexer = new StringIndexer()
-//  .setInputCol("label")
-//  .setOutputCol("indexedLabel")
-//  .fit(data)
-// Automatically identify categorical features, and index them.
-// Set maxCategories so features with > 4 distinct values are treated as continuous.
-//val featureIndexer = new VectorIndexer()
-//  .setInputCol("features")
-//  .setOutputCol("indexedFeatures")
-//  .setMaxCategories(128)
-//  .fit(data)
 
 // Split the data into training and test sets (30% held out for testing).
 val Array(trainingData, testData) = data.randomSplit(Array(0.7, 0.3))
 
 // Train a GBT model.
 
-
-  val lgbm = new LightGBMClassifier()
+  val lgbm = new LightGBMRegressor()
       .setLabelCol("label")
-      .setObjective("binary")
+      .setObjective("regression")
       .setBoostingType("gbdt")
       .setNumLeaves(31)
       .setLearningRate(0.2)
@@ -73,8 +78,8 @@ val Array(trainingData, testData) = data.randomSplit(Array(0.7, 0.3))
       .setBaggingFreq(9)
       .setFeaturesCol("features")
       .setMaxDepth(-1)
-      .setNumIterations(1000)
-      .setIsUnbalance(true)
+      .setNumIterations(2000)
+
 
 //  val paramGrid = new ParamGridBuilder().addGrid(lgbm.lambdaL2, Array(1.0,101.0,10.0)).build
 //
@@ -88,25 +93,44 @@ val Array(trainingData, testData) = data.randomSplit(Array(0.7, 0.3))
   
   val model=lgbm.fit(train)
     
-  model.saveNativeModel("/tmp/mymodel",true)
-  val model2 = LightGBMClassificationModel.loadNativeModelFromFile("/tmp/mymodel")
-
+  model.saveNativeModel("/tmp/mymodelPredict",true)
+  val model2 = LightGBMRegressionModel.loadNativeModelFromFile("/tmp/mymodelPredict")
+  
   val scoredData=model2.transform(test)
   scoredData.show(100,false)
-  val metrics = new ComputeModelStatistics().setEvaluationMetric("classification").setLabelCol("label").setScoredLabelsCol("prediction").transform(scoredData)
+  val metrics = new ComputeModelStatistics().setEvaluationMetric("regression").setLabelCol("label").setScoresCol("prediction").transform(scoredData)
   
-  metrics.show(200,false)
+  metrics.show(20,false)
   
-//                  tp      fn                     eficacia (correctos/total)            exhaustividad = tp/(tp+fn)
-//                  fp      tn                                        tp/(tp+fp)         sensitivity (enfermos correctamente diagnosticados)
-//+---------------+-------------------------------+------------------+------------------+------------------+------------------+
-//|evaluation_type|confusion_matrix               |accuracy          |precision         |recall            |AUC               |
-//+---------------+-------------------------------+------------------+------------------+------------------+------------------+
-//|Classification |165.0    14.0    
-//                  16.0  1518.0                  |0.9824868651488616|0.9908616187989556|0.9895697522816167|0.9556787308894116|
-//+---------------+-------------------------------+------------------+------------------+------------------+------------------+
+//+------------------+-----------------------+------------------+-------------------+
+//|mean_squared_error|root_mean_squared_error|R^2               |mean_absolute_error|
+//+------------------+-----------------------+------------------+-------------------+
+//|1173.8087060092103|34.26089178654301      |0.9833813477043791|16.07175137361102  |
+//+------------------+-----------------------+------------------+-------------------+
   
   
+  
+  val sub=scoredData.rdd.map(s=>math.abs(s.getDouble(0)-s.getDouble(2))).histogram(10)
+  for( i <- 0 to 9){
+         println( sub._1(i) + " - " + sub._2(i) + " - " + (100*sub._2(i)/scoredData.count())+"%" );
+      }
+
+  	
+  	
+//3.548241140833852E-4 - 1409 - 82%
+//26.323244600607588 - 151 - 8%
+//52.64613437710109 - 73 - 4%
+//78.96902415359459 - 34 - 1%
+//105.2919139300881 - 14 - 0%
+//131.6148037065816 - 18 - 1%
+//157.9376934830751 - 9 - 0%
+//184.2605832595686 - 1 - 0%
+//210.58347303606212 - 1 - 0%
+//236.9063628125556 - 3 - 0%
+//Sobre 1713 casos heldout  	
+//  sub.show(1000,false)
+//  
+  println(scoredData.count())
 //  val model = pipelineModel.fit(train)
 //  val preds = model.transform(test).toDF()
   
